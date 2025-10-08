61813a1c46e22a91b9d058ee03dd12ab009de837
diepbh-2305 <bui.huynh.diep@sun-aserisk.com>
2025-10-08 18:11:08 +0700

    xóa 2 file

--- tests/google.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test'; 
-
-test('mở trang chủ Google', async ({ page }) => {
-  // Điều hướng đến trang Google
-  await page.goto('https://www.google.com');
-
-  // Kiểm tra tiêu đề trang có chứa từ "Google"
-  await expect(page).toHaveTitle(/Google/);
-});
